177e0bef437d87083bd1fe717a65278835ecc290
Reid3002 <reid3002@gmail.com>
2024-10-13 18:25:41 -0300

    SpawnFix

--- Battle-Royale/Assets/Scripts/GameController.cs
+++ Battle-Royale/Assets/Scripts/GameController.cs
@@ -7,18 +7,32 @@ public class GameController : MonoBehaviourPunCallbacks
     [SerializeField] private GameObject playerPrefab;
     [SerializeField] private CinemachineVirtualCamera virtualCamera;
 
-    private void Awake()
+    private float waitTime = 0.3f;
+    private float currentWaitTime;
+    private bool initialized = false;
+
+    private void Update()
     {
-        var player = PhotonNetwork.Instantiate(playerPrefab.name, new Vector2(Random.Range(-4, 4), Random.Range(-4, 4)), Quaternion.identity);
-
-
-        player.GetComponent<PlayerController>().OnSpawn += GameManager.Instance.AddPlayer;
-        player.GetComponent<LifeController>().OnDeath += GameManager.Instance.PlayerDeath;
-
-        if (virtualCamera != null)
+        if(currentWaitTime >= waitTime && initialized == false)
         {
-            virtualCamera.Follow = player.transform;
+            var player = PhotonNetwork.Instantiate(playerPrefab.name, new Vector2(Random.Range(-4, 4), Random.Range(-4, 4)), Quaternion.identity);
+
+
+            player.GetComponent<PlayerController>().OnSpawn += GameManager.Instance.AddPlayer;
+            player.GetComponent<LifeController>().OnDeath += GameManager.Instance.PlayerDeath;
+
+            if (virtualCamera != null)
+            {
+                virtualCamera.Follow = player.transform;
+            }
+
+            initialized = true;
         }
+        else
+        {
+            currentWaitTime += Time.deltaTime;
+        }
+        
     }
 
     private void Start()
